360b66af97b7dd4b04aa0299cf02507210dea32f
Andrew Ambrosio <aambrosio@lemongrassconsulting.com>
2021-04-28 16:14:19 +0800

    Implementation of API side, with Postman scripts for testing.

---
+++ HRExam/Service/Salary/RegularSalary.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+using Service.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Service.Salary
+{
+    public class RegularSalary : ISalary
+    {
+        public double ComputeSalary(EmployeeModel employee, double daysPresent, int workDaysInMonth)
+        {
+            var deduction = (employee.Rate / workDaysInMonth) * (workDaysInMonth - daysPresent);
+
+            return employee.Rate - deduction - (employee.Rate * employee.Tax);
+        }
+    }
+}
